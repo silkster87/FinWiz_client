c3721e84742b34315bb1150251ff5796226a8024
Silky Ng <silky.ng@gmail.com>
2022-05-27 11:03:15 +0100

    fix(index): profile endpoint

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''}
-    redirectUri={window.location.origin}
+    redirectUri={`${window.location.origin}/profile`}
   >
     <App />
   </Auth0Provider>,
